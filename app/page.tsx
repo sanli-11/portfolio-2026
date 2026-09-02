63a1909e1e7fd8eb34380be1e0b3c287053f20c4
SanLi <sanli.has11@gmail.com>
2026-09-02 21:53:49 +0500

    Project section initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen w-full">
+    <div className="relative min-h-screen w-full scroll-smooth">
       <Header />
       <div className="px-4">
         <Hero />
+        <Projects />
       </div>
     </div>
   );
---
+++ components/Projects.tsx
@@ -0,0 +1,3 @@
+export default function Projects() {
+  return <div className="min-h-screen w-full bg-red-400" />;
+}
