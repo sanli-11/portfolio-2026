7836f237cbd77b137f8e4545f635bd6dfe813cd5
SanLi <sanli.has11@gmail.com>
2026-09-02 21:51:38 +0500

    Installed lenis and set up smooth scroll

---
+++ components/SmoothScroll.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScroll({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.8,
+      lerp: 0.1,
+      smoothWheel: true,
+      wheelMultiplier: 0.8,
+    });
+
+    let animationFrameId: number;
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      animationFrameId = requestAnimationFrame(raf);
+    };
+
+    animationFrameId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return children;
+}
