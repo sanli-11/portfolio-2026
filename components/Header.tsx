32598aa005900c039f232430f30b14bee4c6d81b
SanLi <sanli.has11@gmail.com>
2026-09-02 10:51:32 +0500

    Created header with skip-content button

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <a
+        href="#"
+        className="fixed -top-16 rounded-br-lg bg-stone-400 px-6 py-2.5 text-black outline-offset-4 outline-red-400 transition-discrete duration-600 hover:top-0 hover:outline-2 focus:top-0 focus:outline-2"
+      >
+        Skip to content
+      </a>
+    </>
+  );
+}
