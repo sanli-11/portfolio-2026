eb01017e341976a4636d115629a1938c705a1173
SanLi <sanli.has11@gmail.com>
2026-09-03 11:03:04 +0500

    Created hgroup for projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,3 +1,19 @@
 export default function Projects() {
-  return <div className="min-h-screen w-full bg-red-400" />;
+  return (
+    <section
+      id="#projects"
+      className="bg-background relative h-screen w-full text-white"
+    >
+      <hgroup className="grid size-full place-content-center text-center">
+        <h2 className="mx-auto max-w-2xl text-7xl/21">
+          Taciti sociosqu ad litora torquent
+        </h2>
+
+        <p className="mx-auto max-w-sm">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit. Faucibus ex
+          sapien vitae pellentesque sem pretium tellus placerat in.
+        </p>
+      </hgroup>
+    </section>
+  );
 }
