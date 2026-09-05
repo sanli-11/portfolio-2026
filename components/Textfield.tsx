2eb49568a2e3ee9774ff820b3c11f8b1968054b0
SanLi <sanli.has11@gmail.com>
2026-09-05 16:29:32 +0500

    Created fields for form

---
+++ components/Textfield.tsx
@@ -0,0 +1,43 @@
+import { type TextareaHTMLAttributes, useState } from "react";
+
+interface TextfieldProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  error?: string;
+}
+
+export default function Textfield({
+  id,
+  label,
+  error,
+  ...props
+}: TextfieldProps) {
+  const [fieldFocused, setFieldFocused] = useState(false);
+
+  return (
+    <section className="group relative">
+      <label
+        htmlFor={id}
+        className="pointer-events-none absolute top-3.25 left-4.25 z-60 transition-all duration-200 group-focus-within:-top-2.5 group-focus-within:backdrop-blur-xs group-has-[input:not(:placeholder-shown)]:-top-2.5"
+      >
+        Project Details
+      </label>
+
+      <textarea
+        id="projectDetails"
+        name="projectDetails"
+        rows={6}
+        placeholder={!fieldFocused ? label : undefined}
+        onFocus={() => setFieldFocused(true)}
+        onBlur={() => setFieldFocused(false)}
+        className="w-full rounded-md border border-zinc-800 bg-zinc-800 px-4 py-3 transition-colors outline-none focus:border-zinc-600"
+        {...props}
+      />
+
+      {error && (
+        <p id={`${id}-error`} className="mt-1 text-sm text-red-400">
+          {error}
+        </p>
+      )}
+    </section>
+  );
+}
